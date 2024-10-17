d04020cbe69c4ca987c1a33565c081a9b3dc8195
laminellas <159630004+laminellas@users.noreply.github.com>
2024-10-17 15:03:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let sections = document.querySelectorAll('section');
+     let navLinks = document.querySelectorAll('header nav a');
+
+     window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+    
+            if(top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                  links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+    let header = documnet.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+}
+let menuIcon = document.querySelector('#menu-icon');
+let nav = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// const activePage = window.location.pathname;
+// const navLinks = document.querySelectorAll('nav a').forEach(link => {
+//     if(link.href.includes(`${activePage}`)){
+//         link.classList.add('active');
+//     }
+// })
